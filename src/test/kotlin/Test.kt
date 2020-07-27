86adbad5dde604c40a917c4fa44d963ec6226c6a
Hahn Jiho <jh.han1@anypointmedia.com>
2020-07-27 14:40:17 +0900

    testPingActor not finished

--- src/test/kotlin/Test.kt
+++ src/test/kotlin/Test.kt
@@ -1,13 +1,11 @@
-import akka.actor.ActorRef
-import akka.actor.ActorSystem
-import akka.actor.Props
+import akka.actor.*
 import akka.testkit.javadsl.TestKit //NOT akka.testkit.TestKit
 import com.typesafe.config.Config
 import com.typesafe.config.ConfigFactory
 import org.junit.Test
 import akka.pattern.Patterns.ask
 import akka.pattern.Patterns.pipe
-import akka.testkit.TestProbe
+import mu.KLogging
 import tv.anypoint.jonathan.actor.*
 import java.time.Duration
 
@@ -15,32 +13,62 @@ import java.time.Duration
 //ask() requires the recipient to reply with 'getSender.tell(reply, self)' -> only for testPongActor?
 
 class TestPingPong() {
-    private val config: Config = ConfigFactory.load("application")
-    private val system: ActorSystem = ActorSystem.create("chat-app", config)
-    private val t: Duration = Duration.ofSeconds(1)
-
     @Test
-    fun testPingActor() {
+    fun testPingActor() {   //not finished..
+        val config: Config = ConfigFactory.load("application")
+        val system: ActorSystem = ActorSystem.create("chat-app", config)
+        val t: Duration = Duration.ofSeconds(1)
         object : TestKit(system){
             init{
-                val startMessage = StartMessage()
-                val probe = TestKit(system)
-                val targetPingActor = system.actorOf(Props.create(PingActor::class.java))
-                val pongActor = system.actorOf(Props.create(PongActor::class.java)) //pong actor not receiving message..
-                pongActor.tell(probe.ref, ref)
+                val targetPingActor: ActorRef = system.actorOf(Props.create(PingActor::class.java))
+                //val pongActor: ActorRef = system.actorOf(Props.create(PongActor::class.java), "pong")
 
-                targetPingActor.tell(startMessage, ActorRef.noSender())
-                probe.expectMsgClass(PingMessage::class.java)
+                val startMessage = StartMessage()
+                val inputProbe = TestKit(system)
+
+                val outputProbe = TestKit(system)
+                targetPingActor.tell(inputProbe.ref, ref)
+                //pongActor.tell(outputProbe.ref, ref)
+
+                /*===thenApply 실행x
+                ask(targetPingActor, startMessage, t).toCompletableFuture()
+                    .thenApply {
+                        inputProbe.expectMsgClass(PingMessage::class.java)  //Should be StartMessage::class.java
+                    }.thenApply {
+                        outputProbe.expectMsgClass(PongMessage::class.java) //should be PingMessage::class.java
+                        println("111")
+                    }
+                 */
+
+                //onComplete.join()     //AskTimeoutException??
+
+                //
+                //targetPingActor.tell(startMessage, ref)
+                //inputProbe.expectMsgClass(PingMessage::class.java)  //should be StartMessage::class.java
+                //---메세지 처리되긴하는데 probe가 못 잡음. testPongActor에선 됐는데 왜 여기선 안 됨?
+                //
+
+                //--daed letter 사용할 수 있나?
+                //val deadLettersRef = system.deadLetters() //why can't I use this?
+                //deadLettersRef.tell(inputProbe.ref, ref)
+                //targetPingActor.tell()
+                //deadLettersRef.
+                //
             }
         }
+        system.terminate()
     }
     @Test
     fun testPongActor() {
+        val config: Config = ConfigFactory.load("application")
+        val system: ActorSystem = ActorSystem.create("chat-app", config)
+        val t: Duration = Duration.ofSeconds(1)
         object : TestKit(system) {
             init {
-                //val probe = TestKit(system)
-                //val pingMessage = PingMessage()
-                val targetPongActor = system.actorOf(Props.create(PongActor::class.java), "pongActor")
+                val testProbe = TestKit(system)
+
+                val pingMessage = PingMessage()
+                val targetPongActor = system.actorOf(Props.create(PongActor::class.java), "pong")
 
                 //===test input
                 //targetPongActor.tell(probe.ref, ref)      //inject probe to targetPongActor
@@ -49,15 +77,17 @@ class TestPingPong() {
 
                 //===test output
                 val future = ask(targetPongActor, PingMessage(), t).toCompletableFuture()
-                pipe(future, this.system.dispatcher()).to(ref)  //'this' necessary?
+                pipe(future, this.system.dispatcher()).to(testProbe.ref)  //'this' necessary?
                 //targetPongActor.tell(pingMessage, ref)    //한 줄로 대체해도 될 듯?
 
-                expectMsgClass(PongMessage::class.java) //test output
+                testProbe.expectMsgClass(PongMessage::class.java)     //should be PongMessage::class.java
             }
         }
+        system.terminate()
     }
+    companion object: KLogging()
 }
 
 fun main(){
-    TestPingPong().testPongActor()
+
 }
